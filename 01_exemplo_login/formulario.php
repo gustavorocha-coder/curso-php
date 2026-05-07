adeca7a8e192446af2d4818580fad7b698c5cf18
gustavorocha-coder <gustavorocha9080@gmail.com>
2026-05-06 22:21:39 -0300

    formulario php
    atualização

---
+++ 01_exemplo_login/formulario.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>login</h1>
+<form action="validar_login.php" method="post">
+    <label>Email:</label>
+    <input type="email" name="email"><br>
+    <label>Senha</label>
+    <input type="password" name="senha"><br>
+    <button type="submit">Acessar</button>
+    <button type="reset">Limpar</button>
+</form>
+
+</body>
+</html>
