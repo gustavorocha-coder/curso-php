3226537ec2f50332694369aacdb4de0a5d0208be
gustavorocha-coder <gustavorocha9080@gmail.com>
2026-05-11 22:22:14 -0300

    cadastro de pessoas
    formulario do pi

---
+++ 02_dados_txt/formulario_pi.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro do usuario</title>
+</head>
+<body>
+    <h1>Cadastro do usuario</h1>
+
+    <label>Nome:</label>
+    <input type="text" name="nome"><br>
+
+    <label>E-mail:</label>
+    <input type="email" name="email"><br>
+
+    <label>Senha:</label>
+    <input type="password" senha="senha"><br>
+    
+    <label>Telefone:</label>
+    <input type="text" name="number"><br>
+
+    <label>Endereço:</label>
+    <input type="text" name="endereço"><br>
+
+    <button type="submit">Cadastrar</button>
+    <button type="reset">Limpar</button>
+
+
+
+
+    
+</body>
+</html>
